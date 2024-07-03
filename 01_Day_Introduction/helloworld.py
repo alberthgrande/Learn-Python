7c04b4c4a0a4286cad64031f7755bf5124df271d
alberthgrande <alberthgrande98@gmail.com>
2024-07-03 13:54:35 +0800

    Updated Learn-Python added 01_Day_Introduction - added helloworld.py

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,21 @@
+# Introduction 
+# Day 1 - 30DaysOfPython Challenge
+
+print(1 + 1)  # addition(+)
+print(1 - 1)  # subtraction(-) 
+print(1 * 1)  # multiplication(*)
+print(1 / 1)  # division(/)
+print(1 ** 1) # exponential(**)
+print(1 % 1)  # modulus(%)
+print(1 // 1) # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                     # Int
+print(type(3.14))                   # Float
+print(type(1 + 3j))                 # Complex
+print(type('Alberth'))              # String
+print(type([1,2,3]))                # List
+print(type({"name" : "Alberth"}))   # Dictionary  
+print(type({9.8, 3.14, 2.7}))       # Set
+print(type((9.8, 3.14, 2.7)))       # Tuple
