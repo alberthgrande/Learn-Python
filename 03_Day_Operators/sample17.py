b84aff8e3429d59492cdeef6853f4d48d4a265f7
alberthgrande <alberthgrande98@gmail.com>
2024-07-04 22:47:35 +0800

    Updated Learn-Python Added sample17

---
+++ 03_Day_Operators/sample17.py
@@ -0,0 +1,8 @@
+# Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+
+hours = int(input("Enter hours: "))
+rate_per_hour = int(input("Enter rate per hour: "))
+
+calculate_pay = hours * rate_per_hour
+
+print("Your weekly earning: ", calculate_pay)
