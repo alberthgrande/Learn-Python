7b11cc934e604cd0a27755219c350a0ecb5c9110
alberthgrande <alberthgrande98@gmail.com>
2024-07-04 22:36:30 +0800

    Updated Learn-Python Added sample14

--- sample14.py
+++
@@ -1,8 +0,0 @@
-# Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
-
-input_num = int(input("Enter a number: "))
-
-if input_num % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
