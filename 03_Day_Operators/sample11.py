9ce172a843f28c02378714a1a58a0e18228ce0b5
alberthgrande <alberthgrande98@gmail.com>
2024-07-04 21:17:18 +0800

    Updated Learn-Python Added sample11

---
+++ 03_Day_Operators/sample11.py
@@ -0,0 +1,8 @@
+# check if "on" is on the string display variables
+str_python = "python"
+str_dragon = "dragon"
+
+# check "on" result
+result = ("on" in str_python) and ("on" in str_dragon)
+
+print(result)
