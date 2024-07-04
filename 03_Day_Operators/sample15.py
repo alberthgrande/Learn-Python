4a662fe0898be58d8ecb049a37f307b5faacd89b
alberthgrande <alberthgrande98@gmail.com>
2024-07-04 22:40:38 +0800

    Updated Learn-Python Added sample15

---
+++ 03_Day_Operators/sample15.py
@@ -0,0 +1,6 @@
+# Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+
+floor_division = 7 // 3
+convert_int = int(floor_division)
+
+print(floor_division == convert_int)
