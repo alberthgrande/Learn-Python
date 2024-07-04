bb910d4a0eeda0982b9c36797f37024d02f0d413
alberthgrande <alberthgrande98@gmail.com>
2024-07-04 21:12:02 +0800

    Updated Learn-Python Added the sample10

---
+++ 03_Day_Operators/sample10.py
@@ -0,0 +1,8 @@
+python_len = len("python")
+dragon_len = len("dragon")
+
+falsy = python_len != dragon_len
+
+print("Python length: ", python_len)
+print("Dragon length: ", dragon_len)
+print("Falsy: ", falsy)
