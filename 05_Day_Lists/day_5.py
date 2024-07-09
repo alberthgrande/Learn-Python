8263f0a09a2fe2cabc950fe35ebc0f55943a781b
alberthgrande <alberthgrande98@gmail.com>
2024-07-09 11:37:05 +0800

    Updated Learn-Python added new sample code from day 5

--- 05_Day_Lists/day_5.py
+++ 05_Day_Lists/day_5.py
@@ -162,7 +162,54 @@ integers = negative_numbers + zero + positive_numbers
 print(integers)
 print("\n")
 
+# join with extend
+num1 = [0, 1, 2, 3]
+num2 = [4, 5, 6]
+num1.extend(num2)
+print("Numbers:", num1)
+print("\n")
+
+negative_numbers = [-5, -4, -3, -2, -1]
+positive_numbers = [1, 2, 3, 4, 5]
+zero = [0]
+
+negative_numbers.extend(zero)
+negative_numbers.extend(positive_numbers)
+print("Integers:", negative_numbers)
+
 fruits = ["banana", "orange", "mango", "lemon"]
 vegetables = ["Tomato", "Potato", "Cabbage", "Onion", "Carrot"]
 fruits_and_vegetables = fruits + vegetables
 print(fruits_and_vegetables)
+print("\n")
+
+# count
+fruits = ["banana", "orange", "mango", "lemon"]
+print("Banana count:", fruits.count("banana"))
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print("Age count:", ages.count(24))
+print("\n")
+
+# index
+fruits = ["banana", "orange", "mango", "lemon"]
+print("Index of banana:", fruits.index("banana"))
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print("Index of age 24: ", ages.index(24))
+print("\n")
+
+# Reverse
+fruits = ["banana", "orange", "mango", "lemon"]
+fruits.reverse()
+print("Revers the fruits: ", fruits)
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.reverse()
+print("Reverse ages:", ages)
+print("\n")
+
+# sort
+fruits = ["banana", "orange", "mango", "lemon"]
+fruits.sort()
+print(fruits)
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.sort()
+print(ages)
