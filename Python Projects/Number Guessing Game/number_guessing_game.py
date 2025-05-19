f93cab268419f985577b67d7ce3b84f458eadda0
alberthgrande <alberthgrande98@gmail.com>
2025-05-19 19:01:20 +0800

    modified : Add tracking for the number of attempts.

--- Python Projects/Number Guessing Game/number_guessing_game.py
+++ Python Projects/Number Guessing Game/number_guessing_game.py
@@ -1,6 +1,19 @@
+import random
+import time
+
+def guess_time(start_time):
+
+    run_time = time.time() - start_time
+    hours, remainder = divmod(run_time, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    if hours >= 1:
+        return f'{int(hours):02}:{int(minutes):02}:{int(seconds):02}'
+    else:
+        return f'{int(minutes):02}:{int(seconds):02}'
+
+
 try:
-    import random
-    import time
 
     print('Welcome to the Number Guessing Game!')
     print('Please select the difficulty level:\n1. Easy (10 chances)\n2. Medium (5 chances)\n3. Hard (3 chances)')
@@ -8,34 +21,25 @@ try:
 
     guess_number = random.randint(1, 101)
     print(guess_number)
-
     start_timer = time.time()
 
 
-    def guess_time(start_time):
-        run_time = time.time() - start_time
-
-        hours, remainder = divmod(run_time, 3600)
-        minutes, seconds = divmod(remainder, 60)
-
-        if hours >= 1:
-            return f'{int(hours):02}:{int(minutes):02}:{int(seconds):02}'
-        else:
-            return f'{int(minutes):02}:{int(seconds):02}'
-
-
     def guess_the_number(chance):
-        while chance > 0:
+        number_of_attempts = 0
 
+        while chance > 0:
             try:
                 guess_input = int(input('Guess a number between 1 and 100: '))
             except ValueError:
                 print('Invalid input! Please enter a valid number.')
                 continue
 
+            number_of_attempts += 1
+
             if guess_input == guess_number:
                 print('🎉 Congrats! You guessed the correct number!')
                 print('⏱ Time taken:', guess_time(start_timer))
+                print('Numbers of attempts:', number_of_attempts)
                 return
             else:
                 print('❌ Sorry, you guessed the wrong number!')
